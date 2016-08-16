66b592a363b6476404d09f684339fe59cba9578b
reza farahani <reza.khmf@gmail.com>
2016-08-16 20:54:47 +1000

    add pod alamfire

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+use_frameworks!
+
+target'HipChatJsonify' do
+         pod 'Alamofire', '~> 3.0'
+
+end
+
---
+++ Pods/Target Support Files/Pods-HipChatJsonify/Pods-HipChatJsonify-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_HipChatJsonifyVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_HipChatJsonifyVersionString[];
+
